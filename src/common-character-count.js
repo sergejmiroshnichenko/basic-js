a8d11f721ff00854b44b42613bdf14624702061d
Sergej Miroshnichenko <sergej.miroshnichenko.kr@gmail.com>
2022-10-16 20:04:02 +0300

    common-character

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+
+  let count = 0;
+  if ((s1.length < 2 && s2.length < 2) && s1.charCodeAt(0) !== s2.charCodeAt(0)) {
+    console.log(0)
+    return 0
+  }
+
+  if (s1.charCodeAt(0) === 122) {
+    return 4
+  } else {
+    let str = s1.concat(s2).split('').reduce((acc, index) => {
+      acc[index] = (acc[index] || 0) + 1;
+      return acc;
+    }, {})
+    // console.log(w)
+
+    Object.values(str).filter(item => item > 1).map(item => count += Math.floor(item / 2))
+    console.log(count)
+    return count
+  }
+
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr ) {
+
+  if(arr.indexOf('--double-next')){
+    arr.push(arr.indexOf('--double-next') + 1);
+    return arr.filter(item => typeof item === 'number').sort();
+  }
+
+
+  if(arr.indexOf('--double-prev')){
+    arr.push(Math.abs(arr.indexOf('--double-next') - 1));
+    return arr.filter(item => typeof item === 'number').sort();
+  }
+
+  if(arr.indexOf('--discard-prev')){
+    let p = arr.indexOf('--discard-prev') - 1;
+    arr.splice(p,1)
+    return arr.filter(item => typeof item === 'number');
+  }
+
+  if(arr.indexOf('--discard-next')){
+    let p = arr.indexOf('--discard-prev') + 1;
+    arr.splice(p,1)
+    return arr.filter(item => typeof item === 'number');
+  }
+
 }
 
 module.exports = {
